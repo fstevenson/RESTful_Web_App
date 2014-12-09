f5804b3e9de52c6c8e483682601ee9093ea84239
Soumya Sundarrajan <soumya.rajan86@gmail.com>
2014-12-09 17:21:16 +0000

    Bash script changed.

--- BashScript/Script.sh
+++ BashScript/Script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-curl -X POST -d @Post_question.txt "http://localhost:3000/questions" --header "Accept: application/json"
+curl -X POST -d @Post_question.txt "http://localhost:3000/questions" --header "Content-Type: application/json"
 
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions/1"
 
-curl -i -H "Accept: application/json" -X PUT -d @Put_question.txt "http://localhost:3000/questions/1"
+#curl -i -H "Content-Type: application/json" -X PUT -d @Put_question.txt "http://localhost:3000/questions/1"
 
 
-curl -X POST  -d @Post_answer.txt "http://localhost:3000/questions/1/answers"  --header "Accept: application/json"
+curl -X POST  -d @Post_answer.txt "http://localhost:3000/questions/1/answers"  --header "Content-Type: application/json"
 
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions/1/answers/1"
 
-curl -i -H "Accept: application/json" -X PUT -d @Put_answer.txt "http://localhost:3000/questions/1/answers/1"
+#curl -i -H "Content-Type: application/json" -X PUT -d @Put_answer.txt "http://localhost:3000/questions/1/answers/1"
 
 
-curl -X POST -d @Post_question_comment.txt "http://localhost:3000/questions/1/comments"  --header "Accept: application/json"
+curl -X POST -d @Post_question_comment.txt "http://localhost:3000/questions/1/comments"  --header "Content-Type: application/json"
 
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions/1/comments/1"
 
-curl -i -H "Accept: application/json" -X PUT -d @Put_question_comment.txt "http://localhost:3000/questions/1/comments/1"
+#curl -i -H "Content-Type: application/json" -X PUT -d @Put_question_comment.txt "http://localhost:3000/questions/1/comments/1"
 
 
-curl -X POST -d @Post_answer_comment.txt "http://localhost:3000/questions/1/answers/1/comments" --header "Accept: application/json"
+curl -X POST -d @Post_answer_comment.txt "http://localhost:3000/questions/1/answers/1/comments" --header "Content-Type: application/json"
 
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions/1/answers/1/comments/2"
 
-curl -i -H "Accept: application/json" -X PUT -d @Put_answer_comment.txt "http://localhost:3000/questions/1/answers/1/comments/2"
+#curl -i -H "Content-Type: application/json" -X PUT -d @Put_answer_comment.txt "http://localhost:3000/questions/1/answers/1/comments/2"
 
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions"
 curl -g -X GET -H "Accept: application/json" "http://localhost:3000/questions/1/answers"
